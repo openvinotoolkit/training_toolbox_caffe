a1e7b1f29eafc40976ad369b9c0c4ba47443789e
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2017-02-22 01:37:12 +0300

    add adhoc MKL support

---
+++ external/mkl/mkl.h
@@ -0,0 +1,8 @@
+#ifndef _MKL_H_
+#define _MKL_H_
+
+#include "mkl_cblas.h"
+#include "mkl_service.h"
+
+#endif
+
---
+++ external/mkl/prepare_mkl.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+# set -ex
+FindLibrary()
+{
+  case "$1" in
+    intel|1)
+      LOCALMKL=`find $DST -name libmklml_intel.so`   # name of MKL SDL lib
+      ;;
+    *)
+      LOCALMKL=`find $DST -name libmklml_gnu.so`   # name of MKL SDL lib
+      ;;
+  esac
+
+}
+
+GetVersionName()
+{
+VERSION_LINE=0
+if [ $1 ]; then
+  VERSION_LINE=`grep __INTEL_MKL_BUILD_DATE $1/include/mkl_version.h 2>/dev/null | sed -e 's/.* //'`
+fi
+if [ -z $VERSION_LINE ]; then
+  VERSION_LINE=0
+fi
+echo $VERSION_LINE  # Return Version Line
+}
+
+# MKL
+DST=`dirname $0`
+OMP=0
+VERSION_MATCH=20160706
+ARCHIVE_BASENAME=mklml_lnx_2017.0.0.20160801.tgz
+MKL_CONTENT_DIR=`echo $ARCHIVE_BASENAME | rev | cut -d "." -f 2- | rev`
+GITHUB_RELEASE_TAG=self_containted_MKLGOLD
+MKLURL="https://github.com/intel/caffe/releases/download/$GITHUB_RELEASE_TAG/$ARCHIVE_BASENAME"
+# there are different MKL lib to be used for GCC and for ICC
+reg='^[0-9]+$'
+VERSION_LINE=`GetVersionName $MKLROOT`
+# Check if MKLROOT is set if positive then set one will be used..
+if [ -z $MKLROOT ] || [ $VERSION_LINE -lt $VERSION_MATCH ]; then
+	# ..if MKLROOT is not set then check if we have MKL downloaded in proper version
+    VERSION_LINE=`GetVersionName $DST/$MKL_CONTENT_DIR`
+    if [ $VERSION_LINE -lt $VERSION_MATCH ] ; then
+      #...If it is not then downloaded and unpacked
+      wget --no-check-certificate -P $DST $MKLURL -O $DST/$ARCHIVE_BASENAME
+      tar -xzf $DST/$ARCHIVE_BASENAME -C $DST
+    fi
+  FindLibrary $1
+  MKLROOT=$PWD/`echo $LOCALMKL | sed -e 's/lib.*$//'`
+fi
+
+# Check what MKL lib we have in MKLROOT
+if [ -z `find $MKLROOT -name libmkl_rt.so -print -quit` ]; then
+  LIBRARIES=`basename $LOCALMKL | sed -e 's/^.*lib//' | sed -e 's/\.so.*$//'`
+  cp $MKLROOT/../mkl.h $MKLROOT/include/mkl.h
+  OMP=1
+else
+  LIBRARIES="mkl_rt"
+fi
+
+
+# return value to calling script (Makefile,cmake)
+echo $MKLROOT $LIBRARIES $OMP
