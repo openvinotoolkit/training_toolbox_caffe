7a00dacd527d5201e83e998838ec8288eba805ae
ArtemySkrebkov <artemy.skrebkov@intel.com>
2017-08-11 14:27:37 +0300

    add reorg yolo layer; add test and test data

---
+++ src/caffe/layers/reorg_yolo_layer.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+
+#include "caffe/layers/reorg_yolo_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void ReorgYoloLayer<Dtype>::Reshape(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+
+    vector<int> top_shape;
+    top_shape.push_back(bottom[0]->shape(0));
+    top_shape.push_back(bottom[0]->shape(1) * stride_ * stride_);
+    top_shape.push_back(bottom[0]->shape(2) / stride_);
+    top_shape.push_back(bottom[0]->shape(3) / stride_);
+
+    top[0]->Reshape(top_shape);
+    CHECK_EQ(top[0]->count(), bottom[0]->count());
+
+    return;
+}
+
+template <typename Dtype>
+void ReorgYoloLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+    ReorgYoloParameter reorg_yolo_param = this->layer_param_.reorg_yolo_param();
+    stride_ = reorg_yolo_param.stride();
+}
+
+
+template <typename Dtype>
+void ReorgYoloLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+
+  int batch = bottom[0]->shape(0);
+  int channels = bottom[0]->shape(1);
+  int height = bottom[0]->shape(2);
+  int width = bottom[0]->shape(3);
+
+  int out_c = channels / (stride_*stride_);
+  for (int b = 0; b < batch; ++b) {
+      for (int c = 0; c < channels; ++c) {
+          for (int y = 0; y < height; ++y) {
+              for(int x = 0; x < width; ++x) {
+                  int in_index  = x + width*y + width*height*c + width*height*channels*b;
+
+                  int c2 = c % out_c;
+                  int offset = c / out_c;
+
+                  int w2 = x*stride_ + offset % stride_;
+                  int h2 = y*stride_ + offset / stride_;
+
+                  int out_index = w2 + width*stride_*h2 + width*stride_*height*stride_*c2 + width*stride_*height*stride_*out_c*b;
+                  top_data[in_index] = bottom_data[out_index];
+              }
+          }
+      }
+  }
+  return;
+}
+
+template <typename Dtype>
+void ReorgYoloLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+    const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+  return;
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(ReorgYoloLayer);
+#endif
+
+INSTANTIATE_CLASS(ReorgYoloLayer);
+REGISTER_LAYER_CLASS(ReorgYolo);
+
+}  // namespace caffe
---
+++ src/caffe/test/test_reorg_yolo_layer.cpp
@@ -0,0 +1,77 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/layers/reorg_yolo_layer.hpp"
+
+#include "caffe/test/test_caffe_main.hpp"
+#include "caffe/test/test_gradient_check_util.hpp"
+
+namespace caffe {
+
+template <typename TypeParam>
+class ReorgYoloLayerTest : public MultiDeviceTest<TypeParam> {
+  typedef typename TypeParam::Dtype Dtype;
+
+ protected:
+  ReorgYoloLayerTest() {
+  }
+
+  virtual void SetUp() {
+  }
+
+  virtual ~ReorgYoloLayerTest() {
+  }
+
+};
+
+//TYPED_TEST_CASE(ReorgYoloLayerTest, TestDtypesAndDevices);
+
+TYPED_TEST_CASE(ReorgYoloLayerTest, ::testing::Types<CPUDevice<float> >);
+
+TYPED_TEST(ReorgYoloLayerTest, onDataFromDarknet) {
+  LayerParameter layer_param;
+  ReorgYoloParameter *reorg_yolo_param = layer_param.mutable_reorg_yolo_param();
+  const int stride = 2;
+
+  const size_t batch = 1;
+  const size_t width = 26, height = 26, channels = 64;
+  const size_t count = batch * width * height * channels;
+
+  reorg_yolo_param->set_stride(stride);
+  std::ifstream in("../src/caffe/test/test_data/reorg_in.data", std::ios::binary);
+  float *inBuf = new float[count];
+  in.read((char *)inBuf, count * sizeof(float));
+  in.close();
+
+  in.open("../src/caffe/test/test_data/reorg_out_ref.data", std::ios::binary);
+  float *outBufRef = new float[count];
+  in.read((char *)outBufRef, count * sizeof(float));
+  in.close();
+
+  vector<Blob<float> *> bottom;
+  Blob<float> *bottomBlob = new Blob<float>(batch, channels,width, height);
+  bottomBlob->set_cpu_data(inBuf);
+  bottom.push_back(bottomBlob);
+
+  vector<Blob<float> *> top;
+  vector<int> shape;
+  shape.push_back(1);
+  shape.push_back(channels * stride * stride);
+  shape.push_back(height / stride);
+  shape.push_back(width  / stride);
+  Blob<float> *topBlob = new Blob<float>(shape);
+  top.push_back(topBlob);
+
+  ReorgYoloLayer<float> layer(layer_param);
+  layer.SetUp(bottom, top);
+  layer.Forward(bottom,top);
+
+  for (int i = 0; i < count; i++) {
+    ASSERT_FLOAT_EQ(outBufRef[i], topBlob->cpu_data()[i]);
+  }
+}
+
+}  // namespace caffe
