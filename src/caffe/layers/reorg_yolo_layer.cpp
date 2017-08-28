079c6f8272e86918a775954e0227b7331c5880af
Druzhkov, Pavel <pavel.druzhkov@intel.com>
2017-08-28 15:11:42 +0300

    remove not-implemented methods

--- src/caffe/layers/reorg_yolo_layer.cpp
+++ src/caffe/layers/reorg_yolo_layer.cpp
@@ -6,9 +6,8 @@
 namespace caffe {
 
 template <typename Dtype>
-void ReorgYoloLayer<Dtype>::Reshape(
-  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
-
+void ReorgYoloLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+                                    const vector<Blob<Dtype>*>& top) {
   vector<int> top_shape;
   top_shape.push_back(bottom[0]->shape(0));
   top_shape.push_back(bottom[0]->shape(1) * stride_ * stride_);
@@ -26,10 +25,9 @@ void ReorgYoloLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   stride_ = reorg_yolo_param.stride();
 }
 
-
 template <typename Dtype>
-void ReorgYoloLayer<Dtype>::Forward_cpu(
-  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+void ReorgYoloLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                                        const vector<Blob<Dtype>*>& top) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
 
@@ -38,20 +36,23 @@ void ReorgYoloLayer<Dtype>::Forward_cpu(
   int height = bottom[0]->shape(2);
   int width = bottom[0]->shape(3);
 
-  int out_channels = channels / (stride_*stride_);
+  int out_channels = channels / (stride_ * stride_);
   for (int ib = 0; ib < batch; ++ib) {
     for (int ic = 0; ic < channels; ++ic) {
       for (int iy = 0; iy < height; ++iy) {
         for (int ix = 0; ix < width; ++ix) {
-          int inIndex  = ix + iy*width + ic*width*height + ib*width*height*channels;
+          int inIndex = ix + iy * width + ic * width * height +
+                        ib * width * height * channels;
 
           int oc = ic % out_channels;
           int offset = ic / out_channels;
 
-          int ow = ix*stride_ + offset % stride_;
-          int oh = iy*stride_ + offset / stride_;
+          int ow = ix * stride_ + offset % stride_;
+          int oh = iy * stride_ + offset / stride_;
 
-          int outIndex = ow + oh*width*stride_ + oc*width*stride_*height*stride_ + ib*width*stride_*height*stride_*out_channels;
+          int outIndex = ow + oh * width * stride_ +
+                         oc * width * stride_ * height * stride_ +
+                         ib * width * stride_ * height * stride_ * out_channels;
           top_data[inIndex] = bottom_data[outIndex];
         }
       }
@@ -61,14 +62,10 @@ void ReorgYoloLayer<Dtype>::Forward_cpu(
 
 template <typename Dtype>
 void ReorgYoloLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
-    const vector<bool>& propagate_down,
-    const vector<Blob<Dtype>*>& bottom) {
+                                         const vector<bool>& propagate_down,
+                                         const vector<Blob<Dtype>*>& bottom) {
   NOT_IMPLEMENTED;
 }
-
-#ifdef CPU_ONLY
-STUB_GPU(ReorgYoloLayer);
-#endif
 
 INSTANTIATE_CLASS(ReorgYoloLayer);
 REGISTER_LAYER_CLASS(ReorgYolo);
