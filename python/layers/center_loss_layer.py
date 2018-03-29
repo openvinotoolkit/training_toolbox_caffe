Fixed gradients

python/layers/center_loss_layer.py
@@ -47,7 +47,7 @@ class CenterLossLayer(caffe.Layer):
         centers = np.array(bottom[2].data).astype(np.float32)
 
         batch_size = bottom[0].num
-        factor = 1.0 / float(batch_size)
+        factor = top[0].diff[0] / float(batch_size)
 
         if propagate_down[0]:
             embeddings_diff = np.zeros(bottom[0].data.shape)
@@ -64,7 +64,7 @@ class CenterLossLayer(caffe.Layer):
             for label in self.sample_id_by_label.keys():
                 for sample_id in self.sample_id_by_label[label]:
                     centers_diff[label] += -factor * embeddings[sample_id]
-            bottom[2].diff[...] = centers_diff
+            bottom[2].diff[...] += centers_diff
 
     def reshape(self, bottom, top):
         top[0].reshape(1)

python/layers/push_loss_layer.py
@@ -52,7 +52,7 @@ class PushLossLayer(caffe.Layer):
             if self.num_valid_pairs > 0.0:
                 embeddings = np.array(bottom[0].data).astype(np.float32)
 
-                factor = 1.0 / float(self.num_valid_pairs)
+                factor = top[0].diff[0] / float(self.num_valid_pairs)
                 for i in xrange(bottom[0].num):
                     for j in xrange(i + 1, bottom[0].num):
                         if self.valid_pairs[i, j]: